a466a8f315b4b176bd4e04b60240e46999fa215f
Igor Cantele <igor.cantele01@universitadipavia.it>
2021-05-30 11:39:14 +0200

    Fix basic net and incomplete aleatory connectivity

--- project.py
+++ project.py
@@ -3,6 +3,7 @@
 <==== BASIC LEVEL ====>
 <=====================>
 """
+"""
 import nest
 
 #setting params
@@ -12,6 +13,11 @@ nest.CopyModel("iaf_psc_alpha", "inh_iaf_psc_alpha")
 exc_syn_dic = {"weight": 20.0}
 inh_syn_dic = {"weight": -30.0}
 
+#inputs
+input_dict = {"weight": 1.2}
+noise_exc1 = nest.Create("poisson_generator", params={"rate": 80000.0})
+noise_exc2 = nest.Create("poisson_generator", params={"rate": 80000.0})
+
 #populations
 excNeuronPop1 = nest.Create("exc_iaf_psc_alpha", 30) 
 excNeuronPop2 = nest.Create("exc_iaf_psc_alpha", 30)
@@ -19,10 +25,8 @@ inhNeuronPop = nest.Create("inh_iaf_psc_alpha", 15)
 #Even changin input of exc pops, I couldn't find any dinamics
 
 #connections
-Ke = .5
-Ki = 2.0
-conn_dict_ex = {"rule": "fixed_indegree", "indegree": Ke}
-conn_dict_in = {"rule": "fixed_indegree", "indegree": Ki}
+nest.Connect(noise_exc1, excNeuronPop1, "all_to_all", syn_spec= input_dict)
+nest.Connect(noise_exc2, excNeuronPop2, "all_to_all", syn_spec= input_dict)
 
 nest.Connect(excNeuronPop1, inhNeuronPop, syn_spec= exc_syn_dic)
 nest.Connect(excNeuronPop2, inhNeuronPop, syn_spec= exc_syn_dic)
@@ -30,32 +34,115 @@ nest.Connect(inhNeuronPop, excNeuronPop1, syn_spec= inh_syn_dic)
 nest.Connect(inhNeuronPop, excNeuronPop2, syn_spec= inh_syn_dic)
 
 #recordings && simulation
-multimeter1 = nest.Create("multimeter", params= {"withtime": True, "record_from":["V_m"]})
-multimeter2 = nest.Create("multimeter", params= {"withtime": True, "record_from":["V_m"]})
-multimeter_inh = nest.Create("multimeter", params= {"withtime": True, "record_from":["V_m"]})
-nest.Connect(multimeter1, excNeuronPop1)
-nest.Connect(multimeter2, excNeuronPop2)
-nest.Connect(multimeter_inh, inhNeuronPop)
+spikeDet1 = nest.Create("spike_detector", 30, params={"withgid": True, "withtime": True})
+spikeDet2 = nest.Create("spike_detector", 30, params={"withgid": True, "withtime": True})
+spikeDet_inh = nest.Create("spike_detector", 15, params={"withgid": True, "withtime": True})
+nest.Connect(excNeuronPop1, spikeDet1)
+nest.Connect(excNeuronPop2, spikeDet2)
+nest.Connect(inhNeuronPop, spikeDet_inh)
 
 nest.Simulate(1000.0)
 
 #plots
 import plotly.express as px
 
-dmm1 = nest.GetStatus(multimeter1, keys= "events")[0]
-Vms1 = dmm1['V_m']
+dmm1 = nest.GetStatus(spikeDet1, keys= "events")[0]
+spikes1 = dmm1['senders']
 ts1 = dmm1["times"]
-plot1 = px.line(x=ts1, y=Vms1, labels={'x': 't', 'y': 'Vm1'})
+plot1 = px.scatter(x=ts1, y=spikes1, labels={'x': 't', 'y': 'spikes 1'})
 plot1.show()
 
-dmm2 = nest.GetStatus(multimeter2, keys= "events")[0]
-Vms2 = dmm2["V_m"]
+dmm2 = nest.GetStatus(spikeDet2, keys= "events")[0]
+spikes2 = dmm2["senders"]
 ts2 = dmm2["times"]
-plot2 = px.line(x=ts2, y=Vms2, labels={'x': 't', 'y': 'Vm2'})
+plot2 = px.scatter(x=ts2, y=spikes2, labels={'x': 't', 'y': 'spikes 2'})
 plot2.show()
 
-dmm_inh = nest.GetStatus(multimeter_inh, keys= "events")[0]
-Vms_inh = dmm_inh["V_m"]
+dmm_inh = nest.GetStatus(spikeDet_inh, keys= "events")[0]
+spikes_inh = dmm_inh["senders"]
 ts_inh = dmm_inh["times"]
-plot3 = px.line(x=ts_inh, y=Vms_inh, labels={'x': 't', 'y': 'Vm_inh'})
+plot3 = px.scatter(x=ts_inh, y=spikes_inh, labels={'x': 't', 'y': 'inh spikes'})
+plot3.show()
+
+"""
+"""
+<===============================>
+<==== ALEATORY CONNECTIVITY ====>
+<===============================>
+"""
+
+import nest
+import numpy as np
+
+#setting params
+edict = { "tau_m": 20.0}
+nest.CopyModel("iaf_psc_alpha", "exc_iaf_psc_alpha", params=edict)
+nest.CopyModel("iaf_psc_alpha", "inh_iaf_psc_alpha")
+
+#inputs
+input_dict = {"weight": 1.2}
+noise_exc1 = nest.Create("poisson_generator", params={"rate": 80000.0})
+noise_exc2 = nest.Create("poisson_generator", params={"rate": 80000.0})
+
+#populations
+excNeuronPop1 = nest.Create("exc_iaf_psc_alpha", 30) 
+
+    #randomizing V_m
+vTh = -55
+vRest = -70
+randomizedInputs = [{"I_e": vRest+(vTh-vRest)*np.random.rand()} for x in excNeuronPop1]
+randomizedVms = [{"V_m": vRest+(vTh-vRest)*np.random.rand()} for x in excNeuronPop1]
+
+nest.SetStatus(excNeuronPop1, randomizedVms)
+excNeuronPop2 = nest.Create("exc_iaf_psc_alpha", 30, params= randomizedVms)
+inhNeuronPop = nest.Create("inh_iaf_psc_alpha", 15)
+    #Even changin input of exc pops, I couldn't find any dinamics
+
+#connections
+d = 1.0
+Je = 12.0
+Ji = -10.0
+conn_dict_ex_str = {"rule": "pairwise_bernoulli", "p": .3}
+conn_dict_ex_weak = {"rule": "pairwise_bernoulli", "p": .05}
+conn_dict_in = {"rule": "pairwise_bernoulli", "p": .3}
+exc_syn_dic = {"delay": d, "weight": Je}
+inh_syn_dic = {"delay": d, "weight": Ji}
+
+nest.Connect(noise_exc1, excNeuronPop1, "all_to_all", syn_spec= input_dict)
+nest.Connect(noise_exc2, excNeuronPop2, "all_to_all", syn_spec= input_dict)
+
+nest.Connect(excNeuronPop1, inhNeuronPop, conn_dict_ex_str, syn_spec= exc_syn_dic)
+nest.Connect(excNeuronPop2, inhNeuronPop, conn_dict_ex_weak, syn_spec= exc_syn_dic)
+nest.Connect(inhNeuronPop, excNeuronPop1, conn_dict_in, syn_spec= inh_syn_dic)
+nest.Connect(inhNeuronPop, excNeuronPop2, conn_dict_in, syn_spec= inh_syn_dic)
+
+#recordings && simulation
+spikeDet1 = nest.Create("spike_detector", 30, params={"withgid": True, "withtime": True})
+spikeDet2 = nest.Create("spike_detector", 30, params={"withgid": True, "withtime": True})
+spikeDet_inh = nest.Create("spike_detector", 15, params={"withgid": True, "withtime": True})
+nest.Connect(excNeuronPop1, spikeDet1)
+nest.Connect(excNeuronPop2, spikeDet2)
+nest.Connect(inhNeuronPop, spikeDet_inh)
+
+nest.Simulate(1000.0)
+
+#plots
+import plotly.express as px
+
+dmm1 = nest.GetStatus(spikeDet1, keys= "events")[0]
+spikes1 = dmm1['senders']
+ts1 = dmm1["times"]
+plot1 = px.scatter(x=ts1, y=spikes1, labels={'x': 't', 'y': 'spikes 1'})
+plot1.show()
+
+dmm2 = nest.GetStatus(spikeDet2, keys= "events")[0]
+spikes2 = dmm2["senders"]
+ts2 = dmm2["times"]
+plot2 = px.scatter(x=ts2, y=spikes2, labels={'x': 't', 'y': 'spikes 2'})
+plot2.show()
+
+dmm_inh = nest.GetStatus(spikeDet_inh, keys= "events")[0]
+spikes_inh = dmm_inh["senders"]
+ts_inh = dmm_inh["times"]
+plot3 = px.scatter(x=ts_inh, y=spikes_inh, labels={'x': 't', 'y': 'inh spikes'})
 plot3.show()
