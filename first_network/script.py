c159137c8ba170543c228cc1e86d6c71498c612f
Igor Cantele <igor.cantele01@universitadipavia.it>
2021-06-20 09:24:56 +0200

    weights-probability interaction

--- first_network/script.py
+++ first_network/script.py
@@ -80,6 +80,42 @@ def label_network(spikeDet, p, dead_sig = None, step_one = None, const_sign = No
     if dead_sig is not None and step_one is not None and const_sign is not None and step_two is not None and exp_sign is not None and step_three is not None:    
         return step_1, step_2, step_3, dead_signal, constant_signal, exploded_signal
 
+def multiple_nets(w, probs_1 = None, probs_20 = None, probs_40 = None, probs_60 = None, probs_80 = None, probs_100 = None):
+    if probs_1 is None and probs_20 is None and probs_40 is None and probs_60 is None and probs_80 is None and probs_100 is None:    
+        global probs_at_1
+        global probs_at_20
+        global probs_at_40
+        global probs_at_60
+        global probs_at_80
+        global probs_at_100
+    else:
+        probs_at_1 = probs_1
+        probs_at_20 = probs_20
+        probs_at_40 = probs_40
+        probs_at_60 = probs_60
+        probs_at_80 = probs_80
+        probs_at_100 = probs_100
+        
+    #recording vars
+    var = nest.GetStatus(spikeDet, keys = "events")
+    spikes = np.array(var[0]["senders"])
+    spike_freq = np.array(spikes * one_sec / time)
+    t = np.array(var[0]["times"])
+    if w == 1:
+        probs_at_1.append(spike_freq)
+    elif w == 20:
+        probs_at_20.append(spike_freq)
+    elif w == 40:
+        probs_at_40.append(spike_freq)
+    elif w == 60:
+        probs_at_60.append(spike_freq)
+    elif w == 80:
+        probs_at_80.append(spike_freq)
+    elif w == 100:
+        probs_at_100.append(spike_freq)
+
+    if probs_1 is not None and probs_20 is not None and probs_40 is not None and probs_60 is not None and probs_80 is not None and probs_100 is not None:    
+        return probs_at_1, probs_at_20, probs_at_40, probs_at_60, probs_at_80, probs_at_100
 
 def plot_add(plot, x_val, y_val, color_graph, name_trace):
     plot.add_trace(go.Scatter(x= x_val, y= y_val, mode= "markers", marker=dict(color=color_graph), name= name_trace))
@@ -94,11 +130,21 @@ dead_signal = np.array([]) #if the outcome doesn't propagate for 9 seconds after
 constant_signal = np.array([]) #if the outcome propagates for at least 9 seconds after the initial pulse
 exploded_signal = np.array([]) #if the outcome at least duplicates itself after 9 seconds after the initial pulse
 weights = []
+probabilities = []
+set_weights = [1, 20, 40, 60, 80, 100]
+probs_at_1 = []
+probs_at_20 = []
+probs_at_40 = []
+probs_at_60 = []
+probs_at_80 = []
+probs_at_100 = []
 
 #data for the plot
 step_1 = []
 step_2 = []
 step_3 = []
+
+    #weights on outcome
 while w < 101.0:
     nest.ResetKernel()    
     input_curr, neuron_pop, spikeDet = create_network(w)
@@ -106,21 +152,33 @@ while w < 101.0:
     label_network(spikeDet, w)
     weights.append(w)
     w += w
-    
+
+    #probability on outcome
 # while p < .104:
 #     nest.ResetKernel()    
 #     input_curr, neuron_pop, spikeDet = create_network(w, p)
 #     nest.Simulate(time)
 #     label_network(spikeDet, p)
+#     probabilities.append(p)
+#     p = .001 + p
+
+    #relationship weight and probability on outcome
+# while p < .104:
+#     while w in set_weights:
+#         nest.ResetKernel()
+#         input_curr, neuron_pop, spikeDet = create_network(w, p)
+#         nest.Simulate(time)
+#         multiple_nets(w) #y axis
+#     probabilities.append(p) #x axis
 #     p = .001 + p
 
 #plotting
 pio.templates.default = "simple_white" # Sets the plotly default theme
 plot = px.scatter()
 
-plot_add(plot, step_1, dead_signal, "rgba(38, 250, 1, .8)", "dead signal")
+plot_add(plot, step_1, dead_signal, "rgba(255, 16, 0, .8)", "dead signal")
 plot_add(plot, step_2, constant_signal, "rgba(250, 220, 0, .8)", "constant signal")
-plot_add(plot, step_3, exploded_signal, "rgba(255, 16, 0, .8)", "exploded signal")
+plot_add(plot, step_3, exploded_signal, "rgba(38, 250, 1, .8)", "exploded signal")
 plot.update_layout(xaxis_title="Weight (pS)", yaxis_title= "Spikes frequency (Hz)")
 plot.show()
 plot.write_image("weight.png")
