1bda4e4013e8a584e257d4e0e444aad9a92e1bdc
Igor10798 <76614313+Igor10798@users.noreply.github.com>
2021-06-24 12:54:58 +0200

    added DataBackup

--- first_network/modules/models.py
+++ first_network/modules/models.py
@@ -2,3 +2,15 @@ class  LineForChart:
     def __init__(self):
         self.x = []
         self.y = []
+        
+import numpy as np
+class DataBackup:
+    def __init__(self):
+        self.dict = {
+            "dead": np.array([]),
+            "constant": np.array([]),
+            "exploded": np.array([]),
+            "time_dead": np.arange(1, sim.time + 1, dtype=int),
+            "time_constant": np.arange(1, sim.time + 1, dtype=int),
+            "time_exploded": np.arange(1, sim.time + 1, dtype=int)
+        }
